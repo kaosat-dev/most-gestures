4a450dbff8aea67715a6ab6760e1d45081f4acf6
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-04-16 16:07:28 +0200

    feat(drags): modified computation of x/y etc params

--- src/drags.js
+++ src/drags.js
@@ -36,9 +36,9 @@ export function mouseDrags (mouseDowns$, mouseUps, mouseMoves, settings) {
 export function touchDrags (touchStarts$, touchEnds$, touchMoves$, settings) {
   const {pixelRatio} = settings
   return touchStarts$
-    .flatMap(function (ts) {
-      let startX = ts.touches[0].pageX * pixelRatio
-      let startY = ts.touches[0].pageY * pixelRatio
+    .flatMap(function (e) {
+      let startX = e.touches[0].pageX * pixelRatio
+      let startY = e.touches[0].pageY * pixelRatio
 
       let prevX = startX
       let prevY = startY
@@ -48,20 +48,16 @@ export function touchDrags (touchStarts$, touchEnds$, touchMoves$, settings) {
           let curX = e.touches[0].pageX * pixelRatio
           let curY = e.touches[0].pageY * pixelRatio
 
-          let x = (curX - startX)
-          let y = (curY - startY)
-
           let delta = {
-            left: x,
-            top: y,
-            x: (prevX - curX),
-            y: (curY - prevY)
+            left: curX - startX,
+            top: curY - startY,
+            x: prevX - curX,
+            y: curY - prevY
           }
 
           prevX = curX
           prevY = curY
 
-          // let output = assign({}, e, {delta})
           const normalized = {x: curX, y: curY}
           return {mouseEvent: e, delta, normalized, type: 'touch'}
         })
